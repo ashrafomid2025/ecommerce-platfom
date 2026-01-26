df4a91c5a7f07ed678956056ea50ecd16f2d5b31
nazi sharifi <banonazisharifi@gmail.com>
2026-01-26 16:56:49 +0430

    addd theme mode

--- components/shared/Toggel-Theme.tsx
+++ components/shared/Toggel-Theme.tsx
@@ -1,15 +1,41 @@
 "use client"
-import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import React, { useEffect, useState } from 'react'
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
 import { useTheme } from 'next-themes'
+import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
 
 function ToggelTheame() {
-    const [themes, setThemes] = useTheme();
+    const {theme, setTheme} = useTheme();
+    const [aneble , setAneble]= useState(false);
+    useEffect(()=>{
+        if(!aneble){
+            setAneble(true);
+        }
+    },[])
   return (
     <DropdownMenu>
         <DropdownMenuTrigger>
-            {}
+            {theme === "light"?(
+                <SunIcon />
+            ):theme==="dark"?(
+                <MoonIcon />
+            ):(
+                <SunMoon/>
+            )}
         </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>Appreance</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem checked={theme==="light"} onClick={()=>setTheme("light")} >
+                Light
+            </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem checked={theme==="dark"} onClick={()=>setTheme("dark")} >
+                Dark
+            </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem checked={theme==="system"} onClick={()=>setTheme("system")} >
+                System
+            </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
     </DropdownMenu>
   )
 }
