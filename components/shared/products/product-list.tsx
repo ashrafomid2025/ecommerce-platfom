8c97df9290e3e5ab8f0cd9d4c27745e6900f47fb
ashrafomid2025 <omidmashraf@gmail.com>
2026-01-28 21:57:07 +0430

    product card added

--- components/shared/products/product-list.tsx
+++ components/shared/products/product-list.tsx
@@ -19,7 +19,7 @@ function ProductList({
       {limitedData.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {limitedData.map((product: any) => (
-            <Product product={product} />
+            <Product key={product.slug} product={product} />
           ))}
         </div>
       ) : (
--- components/shared/products/product.tsx
+++ components/shared/products/product.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -17,6 +17,20 @@ function Product({ product }: { product: any }) {
           />
         </Link>
       </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs">{product.brand}</div>
+        <Link href={`product/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between gap-4">
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <p className="font-bold">{product.price}</p>
+          ) : (
+            <p className="text-destructive">Out Of Stock</p>
+          )}
+        </div>
+      </CardContent>
     </Card>
   );
 }
