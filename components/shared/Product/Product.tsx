1a4ef413837040f4709abb2e75e05e6dce79e038
nazi sharifi <banonazisharifi@gmail.com>
2026-01-29 22:43:36 +0430

    new chenges in code

--- components/shared/Product/Product.tsx
+++ components/shared/Product/Product.tsx
@@ -1,8 +1,29 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-function Product() {
+function Product({product}:{product:any}) {
   return (
-    <div>Product</div>
+      <Card>
+        <CardHeader>
+          <Image src={product.image[0]} alt={product.name} />
+        </CardHeader>
+        <CardContent>
+          <h1>{product.brand}</h1>
+          <Link href={`product/${product.slug}`}>
+          <h1>{product.name}</h1>
+          </Link>
+          <div>
+            <p>{product.rating}Stars</p>
+            {product.stock?(
+              <p>{product.stock}</p>
+            ):(
+              <h1>Out Of Stock</h1>
+            )}
+          </div>
+        </CardContent>
+      </Card>
   )
 }
 
--- components/shared/Product/ProductList.tsx
+++ components/shared/Product/ProductList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Product from './Product'
 
 function ProductList({data,title}:{data:any , title? :string}) {
   return (
@@ -12,9 +13,7 @@ function ProductList({data,title}:{data:any , title? :string}) {
         </div>
         <div className=' w-full grid grid-cols-1 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {data.map((product:any)=>(
-                <div key={product.slug}>
-                    <h1>{product.name}</h1>
-                </div>
+              <Product product={product} />
             ))}
         </div>
     </div>
