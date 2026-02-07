f0b9eabaa277a55b244ade481d59db1ac8dc2ec8
ashrafomid2025 <omidmashraf@gmail.com>
2026-02-07 21:44:15 +0430

    two users seeded

---
+++ app/(root)/seed/page.tsx
@@ -0,0 +1,16 @@
+import sampleData from "@/db/sample-data";
+import { prisma } from "@/lib/db/lib";
+import React from "react";
+
+async function Seed() {
+  await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
+  return <div>seeding</div>;
+}
+
+export default Seed;
