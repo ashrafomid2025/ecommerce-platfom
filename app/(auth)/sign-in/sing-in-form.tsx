a9dde104cbd1a23f8f9b5eab581d6f63a83fd7c7
ashrafomid2025 <omidmashraf@gmail.com>
2026-02-12 10:03:29 +0430

    sign in form added

--- app/(auth)/sign-in/sing-in-form.tsx
+++ app/(auth)/sign-in/sing-in-form.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function singInForm() {
   return (
     <div>
       <Link href=""></Link>
+      <Image src={} alt="" />
     </div>
   );
 }
