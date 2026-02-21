0a0f588795b2e9108e43d3782e9d34c6a313e657
nazi sharifi <banonazisharifi@gmail.com>
2026-02-21 17:06:01 +0430

    add changes

--- lib/action/auth.action.ts
+++ lib/action/auth.action.ts
@@ -1,5 +1,5 @@
 "use server"
-import { authValidationScema, signUpUserValidet } from "../validator";
+import { authValidationScema, productInsertSchema, signUpUserValidet } from "../validator";
 import { signIn, signOut , auth } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
@@ -63,4 +63,36 @@ export async function signInuserWithCredentiols(prevState:unknown , formData: Fo
 // 
 export async function signOutuser(){
     await signOut();
+}
+// function Sign Out end
+// function Insert Product
+
+export async function InsertProduct(prevState: unknown , formData : FormData){
+    try{
+        const ProductInsert = productInsertSchema.parse({
+            name: formData.get("name"),
+            slug: formData.get("slug"),
+            category: formData.get("category"),
+            description: formData.get("description"),
+            brand: formData.get("brand"),
+            banner: formData.get("banner"),
+            stock: formData.get("stock"),
+            isFeacherd: formData.get("isFeacherd"),
+        });
+        await prisma.ecommerePlatform.create({
+            data:{
+                name: ProductInsert.name,
+                slug: ProductInsert.slug,
+                category: ProductInsert.category,
+                description: ProductInsert.description,
+                brand: ProductInsert.brand,
+                banner: ProductInsert.banner,
+                stock: ProductInsert.stock,
+                isFeatured: ProductInsert.isFeacherd,
+            },
+        });
+    }
+    catch(err){
+        return err;
+    }
 }
--- lib/validator.ts
+++ lib/validator.ts
@@ -4,11 +4,12 @@ export const productInsertSchema = z.object({
     name: z.string().min(3, "The Product Name Must be at least 3 Cheracters"),
     slug: z.string().min(5, "The Product Slug Must be at least 5 Cheracters"),
     category: z.string().min(5, "The Product category Must be at least 5 Cheracters"),
-    Description: z.string().min(10, "The Product Description Must be at least 10 Cheracters"),
+    description: z.string().min(10, "The Product Description Must be at least 10 Cheracters"),
     brand: z.string().min(3, "The Product brand Must be at least 3 Cheracters"),
     images: z.array(z.string()).min(1 , "You should select at least 1 image for the product"),
-    bannere: z.string(). nullable(),
+    banner: z.string(). nullable(),
     stock: z.coerce.number(),
+    isFeacherd: z.boolean(),
 })
 export const authValidationScema = z.object({
     email: z.string().email('Invalid email address'),
