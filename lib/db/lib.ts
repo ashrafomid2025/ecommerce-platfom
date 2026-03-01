4b8951b59bf99f7788b3d76858917a08731abba7
ashrafomid2025 <omidmashraf@gmail.com>
2026-03-01 05:59:01 +0430

    postresql db connection issue resolved

--- lib/db/lib.ts
+++ lib/db/lib.ts
@@ -1,10 +1,10 @@
-
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    "postgresql://neondb_owner:npg_d2JiKah3TeYL@ep-broad-brook-a1q4k84l-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&uselibpqcompat=true&channel_binding=require",
 });
 export const prisma =
   globalForPrisma.prisma ||
