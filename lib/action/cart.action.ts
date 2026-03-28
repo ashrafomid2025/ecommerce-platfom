bb11a34fa8e81dbda26d627b78b8d754240bd67a
ashrafomid2025 <omidmashraf@gmail.com>
2026-03-28 20:43:10 +0430

    cart action changed

--- lib/action/cart.action.ts
+++ lib/action/cart.action.ts
@@ -1,10 +1,23 @@
 "use server";
 
+import { auth } from "@/auth";
 import { CartItem } from "@/types";
-
+import { cookies } from "next/headers";
+import { prisma } from "../db/lib";
 export async function AddItemToCart(item: CartItem) {
-  return {
-    success: true,
-    message: "Item added to Cart",
-  };
+  try {
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Session Cart Id Not Found");
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+    const cart = await prisma.cart.findFirst({
+      where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+    });
+    return {
+      success: true,
+      message: "Item added to Cart",
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
